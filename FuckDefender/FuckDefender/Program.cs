28298b7a7308831e87649967cd38e2ea34ba74b7
Alceatraz <alceatraz@blacktech.studio>
2021-08-17 13:31:54 +0800

    0.0.0-Release

---
+++ FuckDefender/FuckDefender/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using Microsoft.Win32;
+
+namespace FuckDefender
+{
+    public static class Program
+    {
+        public static void Main(string[] args) {
+
+            switch (args.Length) {
+
+                case 0:
+                    Remove(new[] {"Windows-Defender"});
+                    break;
+
+                case 1:
+                    if (args[0].Equals("/l")) {
+                        foreach (var subKeyName in ListPackages()) {
+                            Console.Out.WriteLine(subKeyName);
+                        }
+                        break;
+                    }
+                    else {
+                        goto default;
+                    }
+
+                default:
+                    Remove(args);
+                    break;
+            }
+        }
+
+        private static void Remove(string[] searchNames) {
+
+            ArrayList pending = new();
+
+            foreach (var packageName in ListPackages()) {
+                foreach (var searchName in searchNames) {
+                    if (packageName.Contains(searchName)) {
+                        pending.Add(packageName);
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Out.WriteLine("Marked packages: {0}", pending.Count);
+            Console.ResetColor();
+
+            foreach (var packageName in pending) {
+                Console.Out.WriteLine(packageName);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Out.WriteLine("Press Enter to uninstall");
+            Console.ResetColor();
+
+            Console.ReadLine();
+
+            try {
+
+                var currentUser = WindowsIdentity.GetCurrent().User ?? throw new InvalidOperationException("[FAILED] Get current user failed");
+                var packagesRegistry = GetRegistry();
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Out.WriteLine(">> Remove registry");
+                Console.ResetColor();
+
+                foreach (string packageName in pending) {
+
+                    Console.Out.WriteLine(packageName);
+
+                    var packageKey = packagesRegistry.OpenSubKey(packageName, RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.TakeOwnership | RegistryRights.ReadKey | RegistryRights.ReadPermissions) ?? throw new InvalidOperationException("[FAILED] Get Package Key failed " + packageName);
+
+                    var packageKeySecurity = packageKey.GetAccessControl(AccessControlSections.Owner);
+                    packageKeySecurity.SetOwner(currentUser);
+                    packageKeySecurity.AddAccessRule(new RegistryAccessRule(currentUser, RegistryRights.FullControl, AccessControlType.Allow));
+                    packageKey.SetAccessControl(packageKeySecurity);
+
+                    var containsVisibility = packageKey.GetSubKeyNames().Any(packageSubKeyNames => packageSubKeyNames.Equals("Visibility"));
+
+                    if (containsVisibility) {
+                        packageKey.SetValue("Visibility", 0x00000001, RegistryValueKind.DWord);
+                    }
+
+                    var containsOwner = packageKey.GetSubKeyNames().Any(packageSubKeyNames => packageSubKeyNames.Equals("Owners"));
+
+                    if (!containsOwner)
+                        continue;
+
+                    packageKey.DeleteSubKey("Owners");
+
+                    packageKey.Close();
+                }
+
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Out.WriteLine(">> Uninstall package Phase 1");
+                Console.ResetColor();
+                foreach (string packageName in pending) {
+                    Console.Out.WriteLine(packageName);
+                    RemovePackage(packageName);
+                }
+
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Out.WriteLine(">> Uninstall package Phase 2");
+                Console.ResetColor();
+                foreach (string packageName in pending) {
+                    Console.Out.WriteLine(packageName);
+                    RemovePackage(packageName);
+                }
+            }
+            catch (Exception exception) {
+                Console.Out.WriteLine("[FAILED] Exception capture during uninstall");
+                Console.Out.WriteLine(exception);
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Out.WriteLine("Marked package uninstalled");
+            Console.ResetColor();
+
+            Console.Out.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Out.WriteLine("Press Enter to exit");
+            Console.ResetColor();
+
+            Console.ReadLine();
+
+            Console.Out.WriteLine();
+
+        }
+
+
+        private static RegistryKey GetRegistry() {
+            return Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\Packages\") ?? throw new InvalidOperationException("[FAILED] List registry failed");
+        }
+
+        private static IEnumerable<string> ListPackages() {
+            return GetRegistry().GetSubKeyNames();
+        }
+
+        private static void RemovePackage(string packageName) {
+            var process = new Process {
+                StartInfo = {
+                    FileName = "dism.exe",
+                    Arguments = "/online /norestart /remove-package /packagename:" + packageName,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,54 +1,47 @@
-# win6x_registry_tweak v1.4.7
-*This little tool un-hides "packages" like Windows Media Center/Player, IE, IIS, Games, etc... so you can lower the size of your install.wim*
+# FuckDefender
 
-### Important Disclaimer: I did not write this code.  
+**This project is basically steal from [This](https://github.com/shiitake/win6x_registry_tweak)**
 
-#### I've added it to github for posterity sake. It has been floating around for years and was recently mentioned on reddit. It looks like several people have worked on it. Originally it was created by Michał Wnuowski in 2008 and more recent it was worked on by Legolash2o on the [msfn.org forums](https://msfn.org/board/topic/152688-win6x_registry_tweak/). Possibly this github user by the same name https://github.com/Legolash2o.
+## Description
 
-I cannot offer any support or bug fixes; I have not built the project and will most likely not merge any pull requests. Feel free to fork the repo. 
+- Use `dism` instead `pkgmgr`
+- Remove `Win32Security`, Working fine in Win10 21H1 without it
+- Remove offline mode
+- Remove backup mode
+- Remove skip delete mode
+- Remove everything buy only allow remove package named in args
+- Remove Herobrine
 
-##### The latest notes from the forum post above include the following: 
+## Usage
 
-<img src="https://github.com/shiitake/win6x_registry_tweak/blob/master/post-125866-0-02086700-1311877198.png" width="900" />
-Top left is un-hiding a specific package, top right is writing the log of available files to a txt file, bottom left is un-hiding all components of an image and finally the bottom right is un-hiding all the packages from current installed OS.
+### Direct run
 
-*install_wim_tweak.exe /?*  
-This will show all available options..  
+Just double click (**In fact you need `Right Click` And `Run with Administrator`**)  
+It's will uninstall `Windows-Defender` at default args(Or no args).
 
-*install_wim_tweak.exe /p <MountPath>*  
-This will unhide all the packages in the selected image
+### Search With Args
 
-*install_wim_tweak.exe /o*  
-This will unhide all the packages on the currently installed OS
+Args are name search with contains  
+```FuckDefender.exe [name-1] [name-2] [name-3] ...```
 
-*install_wim_tweak.exe /p <MountPath> /l*  
-This will list all the packages available in the selected image and write them to a text file in the same directory.
+### List All Package
 
-*install_wim_tweak.exe /o /l*  
-This will list all the packages available on the installed OS and write them to a text file in the same directory.
+With only one argument `/l` to list all package in your system  
+```FuckDefender.exe /l```
 
-*install_wim_tweaks.exe /p <MountPath> /c Microsoft-Windows-.........*  
-This will just inhide the selected component from the selected image, can also be used with /o. If you add /r at the end it will remove the package.
+## How it work
 
-**Changes made from the original version by wnuku**
+If you directly use
 
-- /h will restore them to default (must use without /h first)
-- /n will not create backups (faster)
-- /d will not delete owners keys.
-- /m is no longer needed, will do the task by default
-- /l will output a list of all packages to a text file.
-- /o will use currently installed image.
-- fixed a bug where it did not work if there was a space in the mountpath.
-- /c <PackageName> will un-hide specific package
-- using /r with /c will remove the package
-- Win32Security.dll file is no longer needed
-- Added new colours, errors are displayed in Red
-- Fixed bug crashing at end of running
-- Fixed bug where it cannot unmount registry if something fails
-- Added specific component selection
-- Fixed some other bugs
-- Added an appropriate small icon for the app
+```
+dism /online /remove-package /package-name:Oh-MaMaMiYa
+```
 
-*Also normally you will have to put the specific component name i.e.
-"/c Microsoft-Hyper-V-Common-Drivers-Package~31bf3856ad364e35~x86~~6.1.7601.17514"
-but if for example you put "/c Microsoft-Hyper-V-Common-Drivers-Package" it will show all packages starting with that.*
+You will got an error code 5, Access Denied. But depends what I learned(Steal in fact) Remove the sub folder(Called RegistryKey in Registry)
+:
+
+```
+HKLM\Microsoft\Windows\CurrentVersion\Component Based Servicing\Packages\Oh-MaMaMiYa\Owner
+```
+
+Then You can remove it by `dism` or `pkgmgr`
